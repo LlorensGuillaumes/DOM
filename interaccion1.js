c4d6e3565268fc8d8ddea8d2aa4ece514e131da3
Joan Llorens Guillaumes <jllorensguillaumes@gmail.com>
2022-12-19 14:23:53 +0100

    Ejerccio1

---
+++ interaccion1.js
@@ -0,0 +1,12 @@
+let classShowme = document.querySelector('.showme')
+console.log(classShowme)
+
+let classIdPillado = document.querySelector('#pillado')
+console.log(classIdPillado)
+
+let todosLosP = document.querySelectorAll('p')
+console.log(todosLosP)
+
+let classPoquemon = document.querySelectorAll('.pokemon')
+console.log(classPoquemon)
+
